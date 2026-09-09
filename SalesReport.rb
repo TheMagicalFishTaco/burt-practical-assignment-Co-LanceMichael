587ce50207c67888c30914bbda0a78f0ee0e3f68
TheMagicalFishTaco <Lance_Michael_Co@outlook.com>
2026-09-09 16:05:21 +0800

    added .csv to the .gitignore, learning how to write to a csv file

--- SalesReport.rb
+++ SalesReport.rb
@@ -1,2 +1,20 @@
+require 'csv'
+
+
 stores_file = File.open("data/stores.json")
 transactions_file = File.open("data/transactions.json")
+
+
+
+
+
+data = [
+{ name: "Alice", age: 30 },
+{ name: "Bob", age: 25 }
+]
+CSV.open("transaction_detail.csv", "wb") do |csv|
+  csv << ["name", "age"] # Write headers
+  data.each do |record|
+    csv << [record[:name], record[:age]]
+  end
+end
