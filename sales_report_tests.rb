27f67c6455ee146d3e8b62591a9a0bf9f18dc98e
TheMagicalFishTaco <Lance_Michael_Co@outlook.com>
2026-09-09 18:13:45 +0800

    basic unit test put in, for testing file loading

--- sales_report_tests.rb
+++ sales_report_tests.rb
@@ -1,7 +1,21 @@
-require "test_helper"
+require 'minitest/autorun'
+require 'tempfile'
+require_relative 'sales_report'
 
-class Test < Minitest::Test
-  def test_
-    
-  end
+=begin
+Test cases to cover
+- file input, make sure that the file exists
+- file input, make sure that the file has the correct data
+
+- data processing, make sure that null/invalid data is handled correctly
+- data processing, cover type errors (integers, floats, strings)
+- data processing, cover CSV injection possibilities
+=end
+
+class LoadFileTests < Minitest::Test
+    def test_asserts_when_file_does_not_exist
+            assert_raises(Errno::ENOENT) do
+              SalesReport.load_json("data/does_not_exist.json")
+            end
+    end
 end
