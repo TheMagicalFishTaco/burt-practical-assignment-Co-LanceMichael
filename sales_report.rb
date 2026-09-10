aa94884eddde51beb670a4efad8080be1c570c42
TheMagicalFishTaco <Lance_Michael_Co@outlook.com>
2026-09-10 10:18:10 +0800

    removed the header comments, properly handling wrong shop IDs

--- sales_report.rb
+++ sales_report.rb
@@ -1,18 +1,6 @@
 require 'csv'
 require 'json'
 
-=begin
-# - `date`
- - `country`
- - `channel`
- - `category`
- - `shop_name`
- - `shop_city`
- - `units_sold`
- - `revenue`
- - `transactions`
-=end
-
 module SalesReport
     SHOP_HEADERS = [:shop_name, :shop_city, :total_units_sold, :total_revenue, :total_transactions].freeze
     TRANSACTION_HEADERS = [:date, :country, :channel, :category, :shop_name, :shop_city, :units_sold, :revenue, :transactions].freeze
@@ -24,23 +12,16 @@ module SalesReport
 
     #turns the stores.json file into a lookup table
     def self.build_shop_lookup(store_data)
-        shops_by_id = store_data.each_with_object({}) do |shop, lookup|
+        store_data.each_with_object({}) do |shop, lookup|
             lookup[shop[:shop_id]] = shop  
         end         
-        #Weird edge case, fix this later to be general edge case handling
-        shops_by_id["S999"] ||= { shop_id: "S999", name: "N/A", city: "N/A"}
-        shops_by_id
     end
 
 
-    #organizes data acquired from transactions.json, for store summary
+    #organizes data acquired from transactions.json by shop_id, for store summary
     def self.group_store_data(transaction_data, shops_by_id)
         transaction_data.group_by { |r| r[:shop_id] }.filter_map do |shop_id, records|
-            shop = shops_by_id[shop_id]
-            if shop.nil?
-                    puts "Skipping unknown"
-                    next  
-            end
+            shop = shops_by_id[shop_id] || {name: "N/A", city: "N/A"}
             {
                 shop_name: shop[:name],
                 shop_city: shop[:city],
@@ -51,25 +32,11 @@ module SalesReport
         end              
     end
 
-=begin
-# - `date`
- - `country`
- - `channel`
- - `category`
- - `shop_name`
- - `shop_city`
- - `units_sold`
- - `revenue`
- - `transactions`
-=end
-
+    #just goes through the list of transactions and puts the data in the right header.
+    #everything but the shop name and shop city can be taken straight from the transaction (r) itself.
     def self.group_transaction_data(transaction_data, shops_by_id)
           transaction_data.filter_map do |r|
-                 shop = shops_by_id[r[:shop_id]]
-                 if shop.nil?
-                    puts "Skipping unknown" 
-                    next
-                 end
+                 shop = shops_by_id[r[:shop_id]] || {name: "N/A", city: "N/A"}
                  {
                     date: r[:date],
                     country: r[:country],
@@ -84,7 +51,7 @@ module SalesReport
           end
     end
 
-    #Generates the CSV File
+    #Generates the CSV File with the requested data and headers
     def self.write_csv(rows, headers, path)
         CSV.open(path, "wb") do |csv|
             csv << headers
